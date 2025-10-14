2751edd4c0260b5ef0181eecbb35824791616714
Christian Orlan Motas <motas.christian9@gmail.com>
2025-10-15 00:39:29 +0800

    Fix13

--- PFTbackend/config/cors.php
+++ PFTbackend/config/cors.php
@@ -1,16 +1,22 @@
 <?php
 
-$frontend = env('FRONTEND_URL', '*');
-$allowed_origins = is_string($frontend) && strpos($frontend, ',') !== false
-    ? array_map('trim', explode(',', $frontend))
-    : [$frontend];
-
 return [
     'paths' => ['api/*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => $allowed_origins,
+
+    'allowed_origins' => [
+        'https://moneytracker2025-4qj5.onrender.com',
+        'http://localhost:5173'
+    ],
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false, // JWT does not need cookies
+
+    'supports_credentials' => false, // false for JWT in headers
 ];
